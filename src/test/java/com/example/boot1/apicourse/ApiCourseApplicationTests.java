d20257e202906d66cc1a11e1c39d7d130ed461e4
Kumar Sanidhya <kumar.sanidhya@infor.com>
2021-04-13 18:50:25 +0530

    1st commit

---
+++ src/test/java/com/example/boot1/apicourse/ApiCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot1.apicourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
